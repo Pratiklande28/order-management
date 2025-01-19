a9905e413a711c7942666c6b69a9dca3f74efca6
Pratiklande28 <pratiklande28@gmail.com>
2025-01-19 11:25:00 +0530

    added exception in new js file

--- new.js
+++ new.js
@@ -1,3 +1,32 @@
 //generate sample javascript code
+//Add signup Functionality
+export const signup= async (email , password) => {
+    try {
+        const response = await fetch('http://localhost:3000/signup',{
+        method : 'POST',
+    headers:{
+'Content-Type':'applicatin/json',
+    },
+    body : JSON.stringify ({email , password}),
+});
+const data = await response.json();
+return data;
+    } catch (error){
+        console.error('Error signin up : ', error);
+
+    }
+        
+
+    };
+/**add signup Exceptions */
+
+
+
+
+
+
+
+
+
 
 
