8360bc7ff1477618e1eeccb8edb03b50bc5eee40
Pratiklande28 <pratiklande28@gmail.com>
2025-01-19 12:02:13 +0530

    push now

--- new.js
+++ new.js
@@ -20,6 +20,24 @@ return data;
     };
 /**add signup Exceptions */
 
+export const signup= async (email , password) => {
+    try {
+        const response = await fetch('http://localhost:3000/signup',{
+        method : 'POST',
+    headers:{
+'Content-Type':'applicatin/json',
+    },
+    body : JSON.stringify ({email , password}),
+});
+const data = await response.json();
+return data;
+    } catch (error){
+        console.error('Error signin up : ', error);
+
+    }
+        
+
+    };
 
 
 
